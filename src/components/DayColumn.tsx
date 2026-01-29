c0488b60748fdc5c5ec57bd85fe7070381e4d138
Luis Gonzalez <lugo50635@gmail.com>
2026-01-29 16:17:32 -0300

    fix: en resoluciones de tablet los items ya no se salen de su cuadro

--- src/components/DayColumn.tsx
+++ src/components/DayColumn.tsx
@@ -5,7 +5,7 @@ import AnimeList from './AnimeList'
 
 function DayColumn ({ day, today, animes } : DayColumnProps) {
   return (
-    <div className={`border-r h-60 lg:h-auto ${today === day ? 'border-r-[#272195] shadow-[0_0_20px_rgba(79,70,229,0.2)] order-first sm:order-0 bg-[#16162D]' : 'border-r-[#2D3343]'}`}>
+    <div className={`border-r min-h-60 ${today === day ? 'border-r-[#272195] shadow-[0_0_20px_rgba(79,70,229,0.2)] order-first sm:order-0 bg-[#16162D]' : 'border-r-[#2D3343]'}`}>
       <header className={`flex justify-between items-center border-b border-b-[#2D3343] p-3 ${today === day ? 'bg-[#1d1d3a]': ''}`}>
         <p className={`font-medium ${today === day ? 'text-[#8B5CF6]' : ''}`}>{day.toUpperCase()}</p>
         <AddAnimeDialog day={day} />
