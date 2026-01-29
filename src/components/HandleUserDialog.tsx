87a2feb8936ad2f45a7ac16321bf1e573b79d9dd
Luis Gonzalez <lugo50635@gmail.com>
2026-01-29 17:05:50 -0300

    change: aplique la nueva fuente Inter

--- src/components/HandleUserDialog.tsx
+++ src/components/HandleUserDialog.tsx
@@ -36,14 +36,14 @@ function HandleUserDialog ({ createUser, label, action } : UserDialogProps) {
           {action === 'edit' ? (
             <div className='flex py-1 items-center gap-3 justify-center hover:bg-gray-100 cursor-pointer'>
               <Pencil size={15} color='#585858' />
-              <p className='text-sm'>
+              <p className='text-sm font-general'>
                 Editar perfil
               </p>
             </div>
           ) : (
             <Button 
               variant="default" 
-              className='cursor-pointer bg-[#8B5CF6]'
+              className='cursor-pointer bg-[#8B5CF6] font-general'
             >
               {label}
             </Button>
@@ -51,27 +51,32 @@ function HandleUserDialog ({ createUser, label, action } : UserDialogProps) {
         </DialogTrigger>
         <DialogContent className="sm:max-w-106.25">
           <DialogHeader>
-            <DialogTitle>{label}</DialogTitle>
-            <DialogDescription>
+            <DialogTitle className='font-general'>{label}</DialogTitle>
+            <DialogDescription className='font-general'>
               Datos de usuario
             </DialogDescription>
           </DialogHeader>
           <div className="grid gap-4">
             <div className="grid gap-3">
-              <Label htmlFor="username">Username</Label>
+              <Label htmlFor="username" className='font-general'>Username</Label>
               <Input id="username" name="username" placeholder="escribe aca tu username" ref={usernameRef} maxLength={20} />
             </div>
             <div className="grid gap-3">
-              <Label htmlFor="avatar">Avatar</Label>
-              <Input id="avatar" name="avatar" placeholder='ingresa la url de la imagen' ref={imageRef} />
+              <Label htmlFor="avatar" className='font-general'>Avatar</Label>
+              <Input 
+                id="avatar" 
+                name="avatar" 
+                placeholder='ingresa la url de la imagen' 
+                ref={imageRef} 
+              />
             </div>
           </div>
           <DialogFooter>
             <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
+              <Button variant="outline" className='font-general'>Cancel</Button>
             </DialogClose>
             <DialogClose asChild>
-              <Button type='submit' onClick={handleCreateUser}>{label}</Button>
+              <Button type='submit' onClick={handleCreateUser} className='font-general'>{label}</Button>
             </DialogClose>
           </DialogFooter>
         </DialogContent>
