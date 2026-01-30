0a2ae3a065bc6271eb5b5ebe313c3bee04d2708b
Luis Gonzalez <lugo50635@gmail.com>
2026-01-30 16:56:23 -0300

    add: se agrego un nuevo step en el tour de la app

--- src/custom-hooks/useTour.ts
+++ src/custom-hooks/useTour.ts
@@ -12,9 +12,11 @@ export function useTour () {
 
     const driverObj = driver({
       showProgress: false,
+      allowClose: false,
       steps: [
         { element: '#cantidadAnimes', popover: { title: 'Cantidad de animes', description: 'Aca puedes ver la cantidad de animes que tienes para ver en el dia actual.', side: "left", align: 'start' }},
         { element: '#user', popover: { title: 'Crear usuario', description: 'Aca vas a poder escoger un nombre de usuario y una foto de perfil.', side: "left", align: 'start' }},
+        { element: '#resetWeek', popover: { title: 'Resetear semana', description: 'Aca puedes resetear todos los animes a estado "no visto", la idea es que lo hagas al inicio de cada nueva semana.', side: "left", align: 'start' }},
         { element: '#addAnimeBtn', popover: { title: 'Agregar anime', description: 'Click aca para añadir un anime. La idea es que agregues el anime al dia en el que salen nuevos capitulos.', side: "left", align: 'start' }},
         { element: '#animeList', popover: { title: 'Lista de Animes', description: 'Aca vas a ver tus animes. Para editarlos o eliminarlos click derecho encima de ellos. En mobile mantenlos presionados.', side: "left", align: 'start' }},
         { popover: { title: 'Ten en cuenta', description: 'Los datos de tus animes y tu usuario se guardan localmente (es decir, en tu navegador), no se comparten entre dispositivos.', side: "left", align: 'start' }},
