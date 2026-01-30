8c251265e261573efcaebfcce435ae8b2b4051df
Luis Gonzalez <lugo50635@gmail.com>
2026-01-30 16:50:33 -0300

    feat: implementacion de la funcion para resetear la semana en el boton de reset del header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { getToday } from '@/utils/getToday'
 import { useAnime } from '@/context/useAnime'
+import { Button } from './ui/button'
+import { RotateCcw } from 'lucide-react'
 import User from './User'
 
 function Header () {
-  const { getAnimeQty } = useAnime()
+  const { getAnimeQty, resetWeekProgress } = useAnime()
   const today = getToday()
   const animeQty = getAnimeQty(today)
 
@@ -14,7 +16,16 @@ function Header () {
         <p className='text-2xl mb-3 sm:mb-0 font-general' id='cantidadAnimes'>
           Hoy es <span className='text-[#8B5CF6]'>{today}</span>, tienes <span className='text-[#8B5CF6]'>{animeQty}</span> anime para ver
         </p>
-        <User />
+        <div className='flex gap-5'>
+          <User />
+          <Button 
+            id='resetWeek'
+            onClick={() => resetWeekProgress()}
+            className='cursor-pointer hover:bg-white hover:text-black transition delay-150 duration-300 ease-in-out'
+          >
+            <RotateCcw />
+          </Button>
+        </div>
       </div>
     </header>
   )
